a0d8ca8f288fa08a89edd7974cc267f07ae2af51
Isaac F <114119376+boxmancoder@users.noreply.github.com>
2023-08-13 00:35:04 +0000

    Excuse generator completed

--- src/app.js
+++ src/app.js
@@ -1,11 +1,46 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const who = [
+  "My Dad",
+  "The Neighbor",
+  "Donald Trump",
+  "The Predator",
+  "The Illuminati",
+  "I unfortunately"
+];
+const what = [
+  "robbed a bank",
+  "fell asleep and never woke up",
+  "had a serious meeting",
+  "took the dog for a walk",
+  "crashed my car"
+];
+const where = [
+  "in my room.",
+  "at Walmart.",
+  "at Burger King.",
+  "in China.",
+  "on my Birthday.",
+  "on December 11th.",
+  "in the alley at Grove Street."
+];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+const excuseSpan = document.getElementById("excuse");
+const generateButton = document.getElementById("generateButton");
+
+function generateRandomExcuse() {
+  const randomWho = who[Math.floor(Math.random() * who.length)];
+  const randomWhat = what[Math.floor(Math.random() * what.length)];
+  const randomWhere = where[Math.floor(Math.random() * where.length)];
+
+  const sentence = `${randomWho} ${randomWhat} ${randomWhere}`;
+  return sentence;
+}
+
+generateButton.addEventListener("click", () => {
+  excuseSpan.textContent = generateRandomExcuse();
+});
+
+// Initial generation
+excuseSpan.textContent = generateRandomExcuse();
